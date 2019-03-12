9106bfc190df4d20993f1d1d7ed2104c1f60aa0f
ssk42 <sthreitz@gmail.com>
2019-03-12 16:57:36 -0400

    Got main section mostly working kind of. We can at least do Blackouts in one, then do the rest later. So yay!
    Signed-off-by: ssk42 <sthreitz@gmail.com>

--- Excel Scripts/Hotels.py
+++ Excel Scripts/Hotels.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import openpyxl
+import numpy
+
+##This is the SurveyGizmo export. This is the master data.
 hotelDF= pd.read_csv('F:/Steve/hotels.csv', encoding='latin1')
 
 ## These identify the proper columns from the original hotel spreadsheet.
@@ -19,4 +22,54 @@ hotelOneDF= pd.merge(hotelNamesDF,hotelOneDF, on=hotelOneDF.index)
 hotelTwoDF= hotelDF[twoIVLP]
 hotelTwoDF= pd.merge(hotelNamesDF,hotelTwoDF, on=hotelTwoDF.index)
 # hotelTwoDF.to_csv('F:/Steve/testTwoHotels.csv')
+
+def hotelName(num):
+	if num==0:
+		return 'Embassy Row Hotel'
+	if num==1:
+		return 'Hampton Inn DC Convention Center'
+	if num==2:
+		return 'The Churchill Hotel'
+	if num==3:
+		return 'The Darcy'
+	if num==4:
+		return 'Residence Inn Washington DC Downtown'
+	if num==5:
+		return 'Homewood Suites by Hilton, Downtown'
+	if num==6:
+		return 'Washington Marriott Georgetown'
+	if num==7:
+		return 'ONE WASHINGTON CIRCLE HOTEL'
+	if num==8:
+		return 'The Fairfax At Embassy Row'
+	if num==9:
+		return 'Washington Plaza Hotel'
+	if num==10:
+		return 'Kimpton Hotel Palomar'
+	if num==11:
+		return 'Kimpton Carlyle Hotel'
+	if num==12:
+		return 'The Donovan'
+	if num==13:
+		return 'Washington Hilton'
+	if num==14:
+		return 'Club Quarters Washington DC'
+	if num==15:
+		return 'Melrose'
+
+## The following section starts the main gist of the program. The first part sets up the dictionary entries per unique IVLP date range.
+dateRangesDF= pd.read_csv('F:/Steve/Hotel Date Ranges.csv')
+dateRangesDF= pd.DataFrame(dateRangesDF['Check-In'])
+for dateName,dateRangeRow in dateRangesDF.iteritems():
+	for name, values in hotelBlackoutDF.iteritems():
+		dateRangeRow={}
+		for x in range(0, 16):
+			# print(x, values[x])
+			if(values[x]=='Blackout dates'):
+				dateRangeRow[x]=hotelName(x)
+				print(dateRangeRow)
+	print(dateName)
+				# print(numpy.linspace(values[x],15))
+
+
 
